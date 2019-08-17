fec5e6d5658379b7f4323258d078f0463c4a10aa
Trey <treyw1850@gmail.com>
2019-08-17 13:20:24 -0500

    login works

--- src/main/java/com/ripe/repositories/UserRepository.java
+++ src/main/java/com/ripe/repositories/UserRepository.java
@@ -1,11 +1,18 @@
 package com.ripe.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ripe.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
-//	public findAllReviewsById(int i)
+
+//    @Query(value = "Select u from User where email=:email and password=:password")
+		User findByEmail(String email);
+		User findByPassword(String password);
+
 }
